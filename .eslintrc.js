7a6e64b1ff0afe5a657aa38f724e221159bc7ab8
Reja Jamil <reja@ganeshait.com>
2024-08-21 12:59:20 +0900

    chore: Prepare

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+// var unusedImports = require('eslint-plugin-unused-imports')
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: ['build/**/*'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'plugin:@next/next/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    // "prettier"
+  ],
+  plugins: [
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+    'unused-imports',
+    // { 'unused-imports': unusedImports },
+    'simple-import-sort',
+  ],
+  rules: {
+    'max-lines': ['error', { max: 500, skipBlankLines: false, skipComments: false }],
+    'prefer-arrow-callback': ['warn', { allowNamedFunctions: false }],
+    'no-duplicate-imports': 'error',
+    'no-console': 'warn',
+    'no-empty': 'off',
+    eqeqeq: 'error',
+    'prettier/prettier': 'error',
+    'array-callback-return': 'warn',
+    'no-unused-vars': 'off',
+    'no-empty-function': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    // '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    // "react-hooks/exhaustive-deps": "error",
+    'react/no-array-index-key': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-unstable-nested-components': [
+      'off',
+      {
+        allowAsProps: false,
+      },
+    ],
+    'react/jsx-key': [
+      'warn',
+      {
+        checkFragmentShorthand: true,
+        checkKeyMustBeforeSpread: true,
+        warnOnDuplicates: true,
+      },
+    ],
+    'react/jsx-no-useless-fragment': [
+      'warn',
+      {
+        allowExpressions: true,
+      },
+    ],
+  },
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,56 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const rewrites = [
+  {
+    source: '/',
+    destination: '/public/home',
+  },
+  {
+    source: '/login',
+    destination: '/auth/login',
+  },
+  {
+    source: '/register',
+    destination: '/auth/register',
+  },
+  {
+    source: '/oke',
+    destination: '/wallet',
+  },
+]
+const redirects = [
+  {
+    source: '/auth/:pathname*',
+    destination: '/:pathname*',
+    permanent: true,
+  },
+]
+
+const nextConfig = {
+  reactStrictMode: true,
+  useFileSystemPublicRoutes: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
+  experimental: {
+    serverSourceMaps: false,
+    // optimizeCss: true
+  },
+  compiler: {
+    // removeConsole: true,
+  },
+  distDir: 'build',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  trailingSlash: false,
+  rewrites: async () => rewrites,
+  redirects: async () => redirects,
+}
+
+export default nextConfig
